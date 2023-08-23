cc77396ed8dfa52aba138e932c14e47b0014d4f7
The Roaming Workshop <63456390+TheRoam@users.noreply.github.com>
2023-08-23 17:49:33 +0200

    Update reinstall philosophy
    Uninstall+install rather than --force-reinstall --no-cache seems to be more stable

--- gpt-prompt/reinstall.sh
+++ gpt-prompt/reinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 source /home/enano/Documents/gpt-prompt/gptP/bin/activate
-
-pip3 install transformers torch --no-cache --force-reinstall
+pip3 uninstall transformers torch
+pip3 install transformers torch
 
 deactivate
