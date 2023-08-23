6468882abb886dbd3c06ad1368e28bc3092ddd5c
The Roaming Workshop <63456390+TheRoam@users.noreply.github.com>
2023-08-23 17:50:03 +0200

    Update reinstall.sh

--- gpt-prompt/reinstall.sh
+++ gpt-prompt/reinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-source /home/enano/Documents/gpt-prompt/gptP/bin/activate
+source ~/Documents/gpt-prompt/gptP/bin/activate
 pip3 uninstall transformers torch
 pip3 install transformers torch
 
